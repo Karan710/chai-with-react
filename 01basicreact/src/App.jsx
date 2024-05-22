05480bca8399e9aa60337461f6d018096df2a204
Karan Puri <karanpuri454@gmail.com>
2024-05-22 11:00:56 +0530

    Used useState

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,7 +1,29 @@
-function App() {
-  return (
+import { useEffect, useState } from "react"
 
+function App() {
+  const [Count, SetCount] =  useState(18)
+
+  const addVal=()=>{
+    if(Count<20){
+      SetCount(Count+1)
+    }
+    else{alert("Cannot exceed 20")}
+  }
+
+  const decVal=()=>{
+    if(Count>0){SetCount(Count-1)}
+    else(alert("Cannot be negitive"))
+  }
+
+  return (
+    <>
     <h1>Chai aur code with Vite. </h1>
+    <h2>Counter Value: {Count} </h2>
+
+    <button onClick={addVal}>Add Value</button>
+    <br />
+    <button onClick={decVal}>Decrease Value</button>
+    </>
   )
 }
 
